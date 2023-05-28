bb199df0909c034c97b7d49b89b82a3926aaf586
arunrawat404 <arunrawatar404@gmail.com>
2023-05-28 22:28:56 +0530

    Added json and urlencoding support for body.req

--- src/index.js
+++ src/index.js
@@ -5,6 +5,14 @@ const apiRoutes = require('./routes');
 
 const app = express();
 
+// app.use() is going to register a middleware for all the upcoming routes that are mentioned below it.
+
+// By default ExpressJS does not know how to read the req.body, we need to mention ExpressJS that in the incoming request, if there is a `req.body` read it like a JSON
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+
 // whenever we get a url that starts with /api will redirect all request to apiRoutes
 app.use('/api', apiRoutes);
 
--- src/routes/v1/airplane_routes.js
+++ src/routes/v1/airplane_routes.js
@@ -5,6 +5,6 @@ const { AirplaneController } = require('../../controllers');
 const router = express.Router();
 
 // POST req on /api/v1/airplanes 
-router.post('/', AirplaneController.createAirplane())
+router.post('/', AirplaneController.createAirplane)
 
 module.exports = router;
