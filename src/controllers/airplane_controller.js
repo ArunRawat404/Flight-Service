b0c94c99e63f5d4fe9f49b8ac4a97e59df323f67
arunrawat404 <arunrawatar404@gmail.com>
2023-05-29 02:15:42 +0530

    Added common success and error object

--- src/controllers/airplane_controller.js
+++ src/controllers/airplane_controller.js
@@ -10,28 +10,23 @@ const { StatusCodes } = require("http-status-codes");
 
 const { AirplaneService } = require('../services');
 
+const { SuccessResponse, ErrorResponse } = require('../utils/common');
+
 async function createAirplane(req, res) {
     try {
         const airplane = await AirplaneService.createAirplane({
             modelNumber: req.body.modelNumber,
             capacity: req.body.capacity
         });
-        return res.status(StatusCodes.OK)
-            .json({
-                success: true,
-                message: "Successfully created an airplane",
-                data: airplane,
-                error: {}
-            })
+        SuccessResponse.data = airplane;
+        console.log(SuccessResponse)
+        return res.status(StatusCodes.CREATED)
+            .json(SuccessResponse);
 
     } catch (error) {
+        ErrorResponse.error = error;
         return res.status(StatusCodes.INTERNAL_SERVER_ERROR)
-            .json({
-                success: false,
-                message: 'Something went wrong while creating airplane',
-                data: {},
-                error: error
-            })
+            .json(ErrorResponse);
     }
 }
 
--- src/middlewares/airplane_middlewares.js
+++ src/middlewares/airplane_middlewares.js
@@ -1,14 +1,13 @@
 const { StatusCodes } = require("http-status-codes");
 
+const { ErrorResponse } = require("../utils/common");
+
 function validateCreateRequest(req, res, next) {
     if (!req.body.modelNumber) {
+        ErrorResponse.message = "Something went wrong while creating airplane";
+        ErrorResponse.error = { explanation: "Model Number not found in the incoming request in correct form" }
         return res.status(StatusCodes.BAD_REQUEST)
-            .json({
-                success: false,
-                message: "Something went wrong while creating airplane",
-                data: {},
-                error: { explanation: "Model Number not found in the incoming request in correct form" }
-            });
+            .json(ErrorResponse);
     }
     // If user sends the Model Number properly without any fail then we will call the next middleware (i.e. the controller) using the next() function
     next();
